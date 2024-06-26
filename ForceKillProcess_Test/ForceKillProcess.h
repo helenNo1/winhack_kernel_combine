35f91c2d1b18200d517d3794b1d25dd38a836f9e
helenNo1 <1460451166@qq.com>
2024-06-26 20:21:03 +0800

    1

---
+++ ForceKillProcess_Test/ForceKillProcess.h
@@ -0,0 +1,70 @@
+#ifndef _FORCE_KILL_PROCESS_H_
+#define _FORCE_KILL_PROCESS_H_
+
+
+#include <ntifs.h>
+
+
+// 定义 SYSTEM_PROCESS_INFORMATION 结构
+typedef struct _SYSTEM_PROCESS_INFORMATION {
+    ULONG NextEntryOffset;
+    ULONG NumberOfThreads;
+    LARGE_INTEGER Reserved[3];
+    LARGE_INTEGER CreateTime;
+    LARGE_INTEGER UserTime;
+    LARGE_INTEGER KernelTime;
+    UNICODE_STRING ImageName;
+    KPRIORITY BasePriority;
+    HANDLE UniqueProcessId;
+    PVOID Reserved2;
+    ULONG HandleCount;
+    ULONG SessionId;
+    ULONG_PTR Reserved3;
+    SIZE_T PeakVirtualSize;
+    SIZE_T VirtualSize;
+    ULONG Reserved4;
+    SIZE_T PeakWorkingSetSize;
+    SIZE_T WorkingSetSize;
+    PVOID Reserved5;
+    SIZE_T QuotaPagedPoolUsage;
+    PVOID Reserved6;
+    SIZE_T QuotaNonPagedPoolUsage;
+    SIZE_T PagefileUsage;
+    SIZE_T PeakPagefileUsage;
+    SIZE_T PrivatePageCount;
+    LARGE_INTEGER Reserved7[6];
+} SYSTEM_PROCESS_INFORMATION, * PSYSTEM_PROCESS_INFORMATION;
+
+typedef enum _SYSTEM_INFORMATION_CLASS {
+    SystemBasicInformation = 0,
+    SystemProcessorInformation = 1,
+    SystemPerformanceInformation = 2,
+    SystemTimeOfDayInformation = 3,
+    SystemProcessInformation = 5,
+    // Other enum values
+} SYSTEM_INFORMATION_CLASS;
+
+// 声明 ZwQuerySystemInformation 函数
+NTSTATUS ZwQuerySystemInformation(
+    SYSTEM_INFORMATION_CLASS SystemInformationClass,
+    PVOID SystemInformation,
+    ULONG SystemInformationLength,
+    PULONG ReturnLength
+);
+
+NTSTATUS GetProcessIdByName(PCWSTR processName, PHANDLE processId);
+
+// 强制结束指定进程
+NTSTATUS ForceKillProcess(HANDLE hProcessId);
+
+// 获取 PspTerminateThreadByPointer 函数地址
+PVOID GetPspLoadImageNotifyRoutine();
+
+// 根据特征码获取 PspTerminateThreadByPointer 数组地址
+PVOID SearchPspTerminateThreadByPointer(PUCHAR pSpecialData, ULONG ulSpecialDataSize);
+
+// 指定内存区域的特征码扫描
+PVOID SearchMemory(PVOID pStartAddress, PVOID pEndAddress, PUCHAR pMemoryData, ULONG ulMemoryDataSize);
+
+
+#endif
